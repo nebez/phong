0e02d8a696310398bbc006789feb2721a399a62e
Dac Chartrand <dac.chartrand@gmail.com>
2016-06-29 09:08:05 -0400

    Paddles should be wider (fixes issue #4) (#5)
    * Paddles should be wider (fixes issue #4)

--- src/CliScreen.php
+++ src/CliScreen.php
@@ -33,19 +33,12 @@ class CliScreen implements Screen
         $c = $entity->getCoordinates();
 
         // Translate coordinates to \CLI\Graphics format
-        if ($c['w'] == 1 && $c['h'] == 1) {
-            $x1 = $c['x'] + 1;
-            $y1 = $c['y'] + 1;
-            $x2 = $x1;
-            $y2 = $y1;
-        } else {
-            $x1 = $c['x'] + 1;
-            $y1 = $c['y'] + 1;
-            $x2 = $c['x'] + $c['w'] - 1;
-            $y2 = $c['y'] + $c['h'];
-        }
-
-        CLI\Graphics::line($x1, $y1, $x2, $y2, [$entity->getDrawingCharacter()]);
+        $x1 = $c['x'] + 1;
+        $y1 = $c['y'] + 1;
+        $x2 = $c['x'] + $c['w'];
+        $y2 = $c['y'] + $c['h'];
+        $dc = $entity->getDrawingCharacter();
+        CLI\Graphics::box($x1, $y1, $x2, $y2, [$dc, $dc, $dc, $dc]);
 
         // Set the cursor to the top left of the window to prevent having
         // a weird cursor flicker follow the last drawn element
