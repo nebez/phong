434f4332063b43990be410de0498804dfa0beb68
Greg Mayes <greg.mayes@fifteendigital.co.uk>
2018-07-27 17:58:03 +0100

    Added a .travis.yml file and a phpunit.xml.dist file
    Downgraded PHPUnit to ^6.5 to ensure that compatibility with PHP 7.0

--- tests/GameTest.php
+++ tests/GameTest.php
@@ -8,64 +8,64 @@ use Phong\State;
 
 class GameTest extends TestCase 
 {
-	/** 
-	 * @var Game 
-	 */
-	protected $game;
+    /** 
+     * @var Game 
+     */
+    protected $game;
 
-	/**
-	 * @return void
-	 */
-	public function setUp()
-	{
-		$this->game = new Game(new State());
-	}
+    /**
+     * @return void
+     */
+    public function setUp()
+    {
+        $this->game = new Game(new State());
+    }
 
-	/** @test */
-	public function a_games_state_is_stopped_by_default()
-	{
-		$this->assertTrue($this->game->getState()->is(State::STOPPED));
-	}
+    /** @test */
+    public function a_games_state_is_stopped_by_default()
+    {
+    	$this->assertTrue($this->game->getState()->is(State::STOPPED));
+    }
 
-	/** @test */
-	public function a_game_can_be_started()
-	{
-		$this->game->setOutput(new CliScreen());
+    /** @test */
+    public function a_game_can_be_started()
+    {
+    	$this->game->setOutput(new CliScreen());
 
-		$this->game->start();
+    	$this->game->start();
 
-		$this->assertTrue($this->game->isRunning());
-	}
+    	$this->assertTrue($this->game->isRunning());
+    }
 
-	/** @test */
-	public function a_game_can_be_paused()
-	{
-		$this->game->pause();
+    /** @test */
+    public function a_game_can_be_paused()
+    {
+    	$this->game->pause();
 
-		$this->assertTrue($this->game->getState()->is(State::PAUSED));
-	}
+    	$this->assertTrue($this->game->getState()->is(State::PAUSED));
+    }
 
-	/** @test */
-	public function a_game_can_be_stopped()
-	{
-		$this->game->setOutput(new CliScreen());
+    /** @test */
+    public function a_game_can_be_stopped()
+    {
+    	$this->game->setOutput(new CliScreen());
 
-		$this->game->start();
+    	$this->game->start();
 
-		$this->assertTrue($this->game->isRunning());
+    	$this->assertTrue($this->game->isRunning());
 
-		$this->game->stop();
+    	$this->game->stop();
 
-		$this->assertTrue($this->game->getState()->is(State::STOPPED));
-	}
+    	$this->assertTrue($this->game->getState()->is(State::STOPPED));
+    }
 
-	/** @test */
-	public function it_correctly_calculates_refresh_rate()
-	{
-		$refreshRate = 60;
+    /** @test */
+    public function it_correctly_calculates_refresh_rate()
+    {
+    	$refreshRate = 60;
 
-		$this->game->setRefreshRate($refreshRate);
+    	$this->game->setRefreshRate($refreshRate);
 
-		$this->assertEquals($this->game->getRefreshRate(), 1000000 / $refreshRate);
-	} 
+    	$this->assertEquals($this->game->getRefreshRate(), 1000000 / $refreshRate);
+    } 
 }
